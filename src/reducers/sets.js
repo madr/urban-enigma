96b1cef5c191f29ec8beb65474b1fa21391340e8
Anders Ytterström <yttan@fastmail.se>
2019-12-14 19:13:42 +0100

    Update existing set from draft

--- src/reducers/sets.js
+++ src/reducers/sets.js
@@ -1,9 +1,18 @@
-import { CREATE_SET, DROP_SET } from "../actions";
+import { CREATE_SET, DROP_SET, UPDATE_SET } from "../actions";
 
 export default (currentState = [], action) => {
   switch (action.type) {
     case CREATE_SET:
       return [...currentState, action.payload.draft];
+    case UPDATE_SET:
+      return currentState.map(set => {
+        const { id: setId } = set;
+        const { id: draftId } = action.payload.draft;
+        if (setId === draftId) {
+          return action.payload.draft;
+        }
+        return set;
+      });
     case DROP_SET:
       return currentState.filter(set => set.id !== action.payload.id);
     default:
